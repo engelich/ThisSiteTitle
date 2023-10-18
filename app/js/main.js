222c2f73595a916312f66fbc94d11354b0690763
Engel Akhmetov <engel.akhmetov@mail.ru>
2023-10-19 04:09:51 +0500

    19.10

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,5 @@
 
+const body = document.querySelector('body');
 const headerBurger = document.querySelector('.header__burger');
 const headerHelp = document.querySelector('.header__help');
 const popup = document.querySelector('.popup');
@@ -6,6 +7,8 @@ const popupClose = document.querySelector('.popup__close');
 const popupBurger = document.querySelector('.burger-menu__container');
 const registrButton = document.querySelector('.burger-menu__button');
 const sumbol = document.getElementById('sumbol');
+const titles = document.querySelectorAll('.accordion__title');
+const contents = document.querySelectorAll('.accordion__content');
 
 
 
@@ -18,9 +21,11 @@ headerHelp.addEventListener('click', () =>  {
 })
 registrButton.addEventListener('click', () =>  {
 	popup.classList.add('popup_active')
+	body.classList.add('header__body_active')
 })
 popupClose.addEventListener('click', () =>  {
 	popup.classList.remove('popup_active')
+	body.classList.remove('header__body_active')
 })
 
 window.addEventListener('keydown', (e) =>{
@@ -28,25 +33,33 @@ window.addEventListener('keydown', (e) =>{
     document.getElementById("popup").classList.remove("popup_active");
 		document.getElementById("burger").classList.remove("burger-menu__container_active");
 		document.getElementById("burger-button").classList.remove("header__burger_active");
+		document.getElementById("body").classList.remove("header__body_active");
   }
 });
 
-const eee = document.querySelectorAll('.accordion__title').forEach((el) =>{
-	el.addEventListener('click', ()=>{
-		let content = el.nextElementSibling;
-		if (content.style.maxHeight){
-			document.querySelectorAll('.accordion__content').forEach((el) => el.style.maxHeight = null)
-			document.querySelectorAll('.accordion__sumbol').forEach((el) => el.style.transform= 'rotate(0deg)')
-		}
 
-		else{
-			document.querySelectorAll('.accordion__content').forEach((el) => el.style.maxHeight = null)
-			content.style.maxHeight = content.scrollHeight + 'px'
-			document.querySelectorAll('.accordion__sumbol').forEach((el) => el.style.transform= 'rotate(180deg)')
-		}
 
-	})
-})
+
+titles.forEach(item => item.addEventListener('click', () => {
+	const activeContent = document.querySelector('#' + item.dataset.tab);
+
+	if (activeContent.classList.contains('active')) {
+			activeContent.classList.remove('active');
+			item.classList.remove('active');
+			activeContent.style.maxHeight = 0;
+	} else {
+			contents.forEach(element => {
+					element.classList.remove('active');
+					element.style.maxHeight = 0;
+			});
+
+			titles.forEach(element => element.classList.remove('active'));
+
+			item.classList.add('active');
+			activeContent.classList.add('active');
+			activeContent.style.maxHeight = activeContent.scrollHeight + 'px';
+	}
+}))
 
 
 
